ca25b2fb79fc627602c897131220ffdbba60deb2
HawkingRadiation <shrey.209303098@muj.manipal.edu>
2022-04-09 02:42:50 +0530

    created __init__ and helper file

---
+++ __init__.py
@@ -0,0 +1,11 @@
+import streamlit as st
+from helpers import *
+from main import skills
+
+st.set_page_config(layout='wide')
+
+st.markdown('''
+<h1> Github Profile README Generator</h1>
+[![GitHub stars](https://img.shields.io/github/stars/rahulbanerjee26/githubProfileReadmeGenerator.svg?style=social&label=Star&maxAge=2592000)](https://github.com/rahulbanerjee26/githubProfileReadmeGenerator/stargazers/)
+
+''', unsafe_allow_html=True)
---
+++ helpers.py
@@ -0,0 +1,11 @@
+import base64
+from unittest import result
+
+def display_skills(skills, github_username):
+    result=[]
+
+    for skill in skills:
+        link = f'https://github.com/{github_username}?tab=repositories&q=&type=&language={skill}&sort='
+        base = f'''<a href= {link} > <img width ='32px' src ='{'https://raw.githubusercontent.com/rahulbanerjee26/githubAboutMeGenerator/main/icons/'+skill+'.svg'}'> </a>'''
+        result.append(base)
+    return '\n'.join(result)
